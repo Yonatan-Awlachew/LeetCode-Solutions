eb6e31239dd1a79ebdf141e0d2fcfbb5f7967deb
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-08-05 10:33:46 +0200

    19.py

---
+++ 19.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+      dummy = ListNode(0,head)
+      left = dummy
+      right = head
+
+      while n>0 and right:
+        n-=1
+        right = right.next
+
+      while right:
+        left=left.next
+        right=right.next
+      
+      left.next = left.next.next
+      
+      return dummy.next
+        
