3e83504d0c6cc852ed69b71fe0c08d41e4d913e9
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-08-08 09:49:54 +0200

    707.py
    It is done using double linked list

--- 707.py
+++ 707.py
@@ -1,64 +1,66 @@
-class Node:
-    def __init__(self, val=0, next=None):
+class ListNode:
+    def __init__(self, val=0,next=None, prev=None):
         self.val = val
-        self.next = next
-
+        self.next= next
+        self.prev=prev
 class MyLinkedList:
 
     def __init__(self):
-        self.head = None
+        self.head = ListNode(0)  # Dummy head
+        self.tail = ListNode(0)
+        self.head.next=self.tail
+        self.tail.prev=self.head
         self.size = 0
 
     def get(self, index: int) -> int:
-        if index < 0 or index >= self.size:
+        if index<0 or index>=self.size:
             return -1
-        curr = self.head
+        curr=self.head.next
         for _ in range(index):
             curr = curr.next
         return curr.val
 
     def addAtHead(self, val: int) -> None:
-        new_node = Node(val, self.head)
-        self.head = new_node
-        self.size += 1
-
+        node = ListNode(val)
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+        self.size+=1
+        
     def addAtTail(self, val: int) -> None:
-        new_node = Node(val)
-        if not self.head:
-            self.head = new_node
-        else:
-            curr = self.head
-            while curr.next:
-                curr = curr.next
-            curr.next = new_node
-        self.size += 1
+        node = ListNode(val)
+        node.next = self.tail
+        node.prev = self.tail.prev
+        self.tail.prev.next = node
+        self.tail.prev = node
+        self.size+=1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index > self.size:
-            return
-        if index <= 0:
-            self.addAtHead(val)
+        if self.size<index or index<0:
             return
         if index == self.size:
             self.addAtTail(val)
             return
-        
-        prev = self.head
-        for _ in range(index - 1):
-            prev = prev.next
-        
-        new_node = Node(val, prev.next)
-        prev.next = new_node
-        self.size += 1
-
-    def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.size:
+        if index ==0:
+            self.addAtHead(val)
             return
-        if index == 0:
-            self.head = self.head.next
-        else:
-            prev = self.head
-            for _ in range(index - 1):
-                prev = prev.next
-            prev.next = prev.next.next
+        node = ListNode(val)
+        curr = self.head.next
+        for _ in range(index):
+            curr = curr.next
+        node.prev = curr.prev
+        node.next = curr
+        curr.prev.next = node
+        curr.prev = node
+        self.size += 1
+        
+    def deleteAtIndex(self, index: int) -> None:
+        if index<0 or index>=self.size:
+            return
+        curr= self.head.next
+        for _ in range(index):
+            curr = curr.next
+        curr.prev.next = curr.next
+        curr.next.prev = curr.prev
         self.size -= 1
