1fb467bd2f7d65ae89311bb8f604d59fe1e4ab7a
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-08-20 07:57:17 +0200

    729.py

--- 729.py
+++ 729.py
@@ -1,25 +1,11 @@
-class TimeMap:
+class MyCalendar:
 
     def __init__(self):
-        self.store={}
+        self.store=[]
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key]=[]
-        self.store[key].append((timestamp,value))
-
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store:
-            return ""
-        values = self.store[key]
-        l,r = 0,len(values)-1
-        result = ""
-        while l<=r:
-            mid = (l+r)//2
-            if values[mid][0]<=timestamp:
-                result = values[mid][1]
-                l = mid+1
-            else:
-                r=mid-1
-        return result
-
+    def book(self, startTime: int, endTime: int) -> bool:
+        for i,j in self.store:
+            if startTime<j and endTime>i:
+                return False
+        self.store.append((startTime,endTime))
+        return True
