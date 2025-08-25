223150016776806a792268a724bd3c5b6a097430
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-08-25 07:48:53 +0200

    146.py

--- 146.py
+++ 146.py
@@ -48,11 +48,3 @@ class LRUCache:
             new = Node(key,value)
             self.add(new)
             self.cache[key] = new
-            
-
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
