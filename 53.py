2de29df4fbd60129da1cec72dc72c254f8d0c1c3
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-09-10 08:24:49 +0200

    53.py

--- 53.py
+++ 53.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = nums[0]
+        max_sum = nums[0]
         curr = 0
         for n in nums:
-            curr = max(0,curr)
-            curr+=n
-            maxSum = max(curr,maxSum)
-        return maxSum
+            curr = max(curr,0) + n
+            max_sum = max(curr,max_sum)
+        return max_sum
