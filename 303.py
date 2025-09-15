e5d2677213b6fa648270b3f2afa859d232487085
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-09-15 08:09:56 +0200

    303.py

---
+++ 303.py
@@ -0,0 +1,13 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = []
+        total = 0
+        for num in nums:
+            total+=num
+            self.prefix.append(total)
+
+    def sumRange(self, left: int, right: int) -> int:
+        left_sum = self.prefix[left-1] if left>0 else 0
+        right_sum = self.prefix[right]
+        return right_sum-left_sum
