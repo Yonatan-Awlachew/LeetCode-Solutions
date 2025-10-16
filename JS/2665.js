9c4f625361528990812f1a84053ff57b353fe6e9
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-10-16 08:34:01 +0200

    2665.js

---
+++ JS/2665.js
@@ -0,0 +1,23 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+
+    function increment(){
+        return ++count;
+    }
+    function decrement(){
+        return --count;
+    }
+    function reset(){
+        count = init;
+        return count;
+    }
+    return {
+        increment:increment,
+        decrement:decrement,
+        reset:reset
+    }
+};
