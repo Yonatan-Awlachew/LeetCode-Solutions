aa6c555f2df9bd13fd2499d083da0a44a45fe228
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-10-27 08:23:44 +0100

    RISC-V program to remove spaces from input
    This assembly program prompts the user to enter text, removes spaces from the input, and then prints the result.

---
+++ RISC-V/remove_space.asm
@@ -0,0 +1,55 @@
+.eqv Print_int,1
+.eqv Print,4
+.eqv Read,8
+.eqv Exit,93
+
+.data
+message1: .asciz "Enter any Text : "
+buf: .space 256
+result: "Result: "
+
+.text
+#main:
+	la a0,message1
+	li a7,Print
+	ecall
+	
+	la a0,buf
+	li a1,256
+	li a7,Read
+	ecall
+	
+	la a0,result
+	li a7,Print
+	ecall
+	
+	la a0,buf
+	jal remove_space
+		
+	
+	la a0,buf
+	li a7,Print
+	ecall
+	
+	li a0,0
+	li a7,Exit
+	ecall
+
+remove_space:
+	mv t0,a0
+	mv t1,a0
+	li t3,' '
+loop:
+	lbu t2,0(t0)
+	addi t0,t0,1
+	beqz t2,end
+	
+	beq t2,t3,loop
+	
+	sb t2,0(t1)
+	addi t1,t1,1
+	j loop
+	
+end:
+	sb zero,0(t1)
+	ret
