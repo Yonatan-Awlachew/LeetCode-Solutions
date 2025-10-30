f416ee1d48624a78523d628f804fcb4820fee10b
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-10-30 21:56:00 +0100

    String length counting in RISC-V assembly

--- RISC-V/count_string.asm
+++ RISC-V/count_string.asm
@@ -1,54 +1,52 @@
-.eqv Print_int,1
-.eqv Print,4
-.eqv Read,8
-.eqv Exit,93
-
-.data
-message1: .asciz "Enter any Text : "
-buf: .space 256
-result: "Length: "
-
-.text
-main:
-	la a0,message1
-	li a7,Print
-	ecall
+# Count length of Text
+	.eqv	Print_int,	1
+	.eqv	Print,		4
+	.eqv	Read,		8
+	.eqv	Exit,		93
 	
-	la a0,buf
-	li a1,256
-	li a7,Read
-	ecall
+	.data
+		buf:		.space		256
+		prompt1:	.asciz		"Hello, Write any text : "
+		prompt2:	.asciz		"Length : "
 	
-	la a0,result
-	li a7,Print
-	ecall
-	
-	la a0,buf
-	jal count
+	.text
+		main:
+			la 	a0,	prompt1
+			li 	a7,	Print
+			ecall	
+			
+			la	a0,	buf
+			li	a1,	100
+			li 	a7,	Read
+			ecall
+			
+			la	a0,	prompt2
+			li	a7,	Print
+			ecall
+			
+			la	a0,	buf
+			jal	count
+			
+			mv	a0,	t1
+			li	a7,	Print_int
+			ecall
+			
+			li	a0,	0
+			li	a7,	Exit
+			ecall
 		
-	
-	mv a0,t1
-	li a7,Print_int
-	ecall
-	
-	li a0,0
-	li a7,Exit
-	ecall
-
-count:
-	mv t0,a0
-	li t1,0
-	li t3,'\n'
-loop:
-	lbu t2,0(t0)
-	beqz t2,end
-	
-	beq t2,t3,end
-	
-	addi t0,t0,1
-	addi t1,t1,1
-	j loop
-	
-end:
-	mv a0,t1
-	ret
+		count:
+			mv	t0,	a0
+			li	t1,	0
+			li	t3, '\n'
+		
+		loop:
+			lbu	t2,	0(t0)
+			addi	t0,	t0,	1
+			beqz	t2,	done
+			
+			beq	t2,	t3,	done
+			addi	t1,	t1,	1
+			j loop
+		done:
+			ret
