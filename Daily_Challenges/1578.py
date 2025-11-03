2ef5e363b187af850f1da0e1b4071f67aa972ec5
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2025-11-03 07:50:15 +0100

    Create 1578.py

---
+++ Daily_Challenges/1578.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        tot_time,i = 0,0
+        while i<len(colors):
+            j = i 
+            group_sum,group_max = 0,0
+            while j<len(colors) and colors[j]==colors[i]:
+                group_sum += neededTime[j]
+                group_max = max(group_max,neededTime[j])
+                j+=1
+            
+            tot_time+=group_sum-group_max
+            i=j
+        return tot_time
