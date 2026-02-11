ed50ea7873c1fa5be68a332c91bc53e1787993ae
Yonatan Firde <117796145+Yonatan-Awlachew@users.noreply.github.com>
2026-02-11 22:30:50 +0100

    Check if Every Row and Column Contains All Numbers

---
+++ Python/2133.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        n=len(matrix)
+        target = set(range(1,n+1))
+        for i in range(n):
+            if set(matrix[i])!=target: return False
+            if set(matrix[j][i] for j in range(n))!=target: return False
+        return True
